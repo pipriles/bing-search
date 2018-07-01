4fa6698d0b949aa519eca06f2cc527fbf335b2f7
Oswaldo Capriles <oswald.capriles46@gmail.com>
2018-06-30 22:37:15 -0400

    Fixed retro feedback script

--- keywords.py
+++ keywords.py
@@ -40,17 +40,20 @@ def keywords_search(site, keywords=[]):
 
 def prepare_input(filename, keywords=[]):
 
-    df1 = pd.read_csv(filename, dtype=str)
-    df1 = df1.iloc[:, 0] # Assume first column has the sites
-    df2 = pd.DataFrame(index=df1.index, columns=keywords)
+    columns = [ 'Domain', *keywords, '_count' ]
+    df = pd.read_csv(filename, dtype=str)
+    
+    df = [ df.get(k, pd.Series(index=df.index, name=k)) \
+            for k in columns ]
 
-    return pd.concat([df1, df2], axis=1)
+    return pd.concat(df, axis=1)
 
 def search_websites(filename):
 
     keywords = [ "teelaunch", "pillow profits", 
         "printify", "printful", "customcat"]
 
+    print('Processing input...')
     websites = prepare_input(filename, keywords)
     length = len(websites)
     results = []
@@ -61,7 +64,7 @@ def search_websites(filename):
             print('-', row[0])
 
             # Work just first column
-            kcolumns = row[1:].notna()
+            kcolumns = row[1:-1].notna()
             if kcolumns.any(): continue
 
             site = row[0]
@@ -71,8 +74,17 @@ def search_websites(filename):
     finally:
         # Write that shit
         columns = [ 'Domain', *keywords, '_count' ]
-        df = pd.DataFrame(results)
-        df[columns].to_csv('results.csv', index=None)
+        offset = len(results)
+
+        # Order columns
+        df1 = pd.DataFrame(results, dtype=str)
+        df1 = df1[columns]
+
+        # Put not scraped sites
+        df2 = websites.iloc[offset:,:]
+
+        df = pd.concat([df1, df2])
+        df.to_csv('results.csv', index=None)
 
 def main():
 
